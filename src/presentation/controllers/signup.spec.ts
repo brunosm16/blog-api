923d91282d7615b78c76c2435e0c349db0e680f8
brunosm16 <brunoskr23@gmail.com>
2022-12-31 23:33:37 -0300

    test: return 400 if no passwordConfirm is provided for sign-up-controller

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -55,4 +55,23 @@ describe('SignUpController Tests', () => {
     expect(statusCode).toBe(400)
     expect(body).toEqual(expectedError)
   })
+
+  it('should return 400 status code if no password is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'Lorem Ipsum',
+        email: 'loremipsum@email.com',
+        password: 'loremipsum123@#'
+      }
+    }
+
+    const { statusCode, body } = sut.handle(httpRequest)
+
+    const expectedError = new MissingParamError('passwordConfirm')
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual(expectedError)
+  })
 })
