e33d024ba799e22c90db3877891d847e3fa50969
brunosm16 <brunoskr23@gmail.com>
2023-01-01 13:03:31 -0300

    feat: calls add-account with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,12 +1,15 @@
+import { AddAccount } from '../../domain/usecases/add-account'
 import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { makeBadRequest, makeInternalServerError, makeOKRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -21,7 +24,7 @@ export class SignUpController implements Controller {
         }
       }
 
-      const { email, password, passwordConfirm } = httpRequest.body
+      const { name, email, password, passwordConfirm } = httpRequest.body
 
       const invalidPasswords = password !== passwordConfirm
 
@@ -34,6 +37,8 @@ export class SignUpController implements Controller {
       if (!emailIsValid) {
         return makeBadRequest(new InvalidParamError('email'))
       }
+
+      this.addAccount.add({ name, email, password })
 
       return makeOKRequest()
     } catch (err) {
