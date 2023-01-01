13bbac61713725de4a246e354d1017434ffc1ca3
brunosm16 <brunoskr23@gmail.com>
2022-12-31 21:40:31 -0300

    test: return 400 if no email is provided for sign-up-controller

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,4 +18,22 @@ describe('SignUpController Tests', () => {
     expect(statusCode).toBe(400)
     expect(body).toEqual(expectedError)
   })
+
+  it('should return 400 status code if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'Lorem Ipsum',
+        password: 'loremipsum123@#'
+      }
+    }
+
+    const { statusCode, body } = sut.handle(httpRequest)
+
+    const expectedError = new Error('Missing param: email')
+
+    expect(statusCode).toBe(400)
+    expect(body).toEqual(expectedError)
+  })
 })
