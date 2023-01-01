fcbe21ea3d423d39050cbfb75d5e0fac7e06f14a
brunosm16 <brunoskr23@gmail.com>
2022-12-31 21:37:11 -0300

    feat: validate required fields for sign-up-controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,15 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    const { body } = httpRequest
+    const requiredFields = ['name', 'email', 'password']
+
+    for (const field of requiredFields) {
+      if (!body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing param: ${field}`)
+        }
+      }
     }
   }
 }
