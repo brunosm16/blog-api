0b0565e63c6f36ba6640bda0a276a85b94b377b1
brunosm16 <brunoskr23@gmail.com>
2022-12-31 23:30:48 -0300

    refactor: reuse code with http-helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,25 +1,19 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { makeBadRequest, makeOKRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     const { body } = httpRequest
+
     const requiredFields = ['name', 'email', 'password']
 
     for (const field of requiredFields) {
-      const responseError = {
-        statusCode: 400,
-        body: new MissingParamError(field)
-      }
-
       if (!body[field]) {
-        return responseError
+        return makeBadRequest(new MissingParamError(field))
       }
     }
 
-    return {
-      statusCode: 200,
-      body: {}
-    }
+    return makeOKRequest()
   }
 }
