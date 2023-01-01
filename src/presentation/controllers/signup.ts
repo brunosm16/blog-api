cd5b740c3823a521011f388191e98090c3789ed1
brunosm16 <brunoskr23@gmail.com>
2023-01-01 00:11:15 -0300

    feat: returns 500 if occurs a server error

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,10 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { InvalidParamError } from '../errors/invalid-param-error'
-import { makeBadRequest, makeOKRequest } from '../helpers/http-helper'
+import { makeBadRequest, makeInternalServerError, makeOKRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
+import { ServerError } from '../errors/server-error'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -13,24 +14,28 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const { body } = httpRequest
+    try {
+      const { body } = httpRequest
 
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
 
-    for (const field of requiredFields) {
-      if (!body[field]) {
-        return makeBadRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!body[field]) {
+          return makeBadRequest(new MissingParamError(field))
+        }
       }
+
+      const { email } = httpRequest.body
+
+      const emailIsValid = this.emailValidator.isValid(email)
+
+      if (!emailIsValid) {
+        return makeBadRequest(new InvalidParamError('email'))
+      }
+
+      return makeOKRequest()
+    } catch (err) {
+      return makeInternalServerError(new ServerError())
     }
-
-    const { email } = httpRequest.body
-
-    const emailIsValid = this.emailValidator.isValid(email)
-
-    if (!emailIsValid) {
-      return makeBadRequest(new InvalidParamError('email'))
-    }
-
-    return makeOKRequest()
   }
 }
