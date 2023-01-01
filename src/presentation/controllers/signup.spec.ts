c0803c68c8008ce6696cbd180ef8dcad322590b1
brunosm16 <brunoskr23@gmail.com>
2022-12-31 21:13:32 -0300

    test: returns 400 if no email provided in sign-up-controller

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,19 +1,19 @@
-import { SignUpController } from "./signup";
+import { SignUpController } from './signup'
 
-describe("SignUpController Tests", () => {
-  it("should return 400 status code if no name is provided", () => {
-    const sut = new SignUpController();
+describe('SignUpController Tests', () => {
+  it('should return 400 status code if no name is provided', () => {
+    const sut = new SignUpController()
 
     const httpRequest = {
       body: {
-        name: "Lorem Ipsum",
-        email: "loremipsum@email.com",
-        password: "loremipsum123@#",
-      },
-    };
+        name: 'Lorem Ipsum',
+        email: 'loremipsum@email.com',
+        password: 'loremipsum123@#'
+      }
+    }
 
-    const { statusCode } = sut.handle(httpRequest);
+    const { statusCode } = sut.handle(httpRequest)
 
-    expect(statusCode).toBe(400);
-  });
-});
+    expect(statusCode).toBe(400)
+  })
+})
