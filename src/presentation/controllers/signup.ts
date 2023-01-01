2c9c04fe1d306256ae6afc438169c29cfe9104fc
brunosm16 <brunoskr23@gmail.com>
2022-12-31 23:54:07 -0300

    feat: validate email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { makeBadRequest, makeOKRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const { body } = httpRequest
 
@@ -13,6 +21,14 @@ export class SignUpController implements Controller {
       if (!body[field]) {
         return makeBadRequest(new MissingParamError(field))
       }
+    }
+
+    const { email } = httpRequest.body
+
+    const emailIsValid = this.emailValidator.isValid(email)
+
+    if (!emailIsValid) {
+      return makeBadRequest(new InvalidParamError('email'))
     }
 
     return makeOKRequest()
