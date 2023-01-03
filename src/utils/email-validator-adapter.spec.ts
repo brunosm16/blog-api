feff59fc2ab68533ceb0ba505dd5d7419ab379d6
brunosm16 <brunoskr23@gmail.com>
2023-01-03 15:24:41 -0300

    test: mock isEmail method from validator

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,10 +1,19 @@
+import validator from 'validator'
 import { EmailValidatorAdapter } from './email-validator-adapter'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidatorAdapter Tests', () => {
   it('should return false if email is invalid', () => {
     const sut = new EmailValidatorAdapter()
 
-    const fakeEmail = 'loremipsum'
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
+    const fakeEmail = 'loremipsum@email.com'
 
     const result = sut.isValid(fakeEmail)
 
