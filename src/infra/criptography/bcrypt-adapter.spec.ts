48e0bda86dc5381c32a54ce3ce00c728cf472419
brunosm16 <brunoskr23@gmail.com>
2023-01-04 10:47:18 -0300

    test: return a hash on bcrypt-adapter success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,11 +1,19 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+const FAKE_HASH = 'fake_hash'
+
 const makeSut = (salt = 12): BcryptAdapter => {
   const sut = new BcryptAdapter(salt)
 
   return sut
 }
+
+jest.mock('bcrypt', () => ({
+  async hash (value: string): Promise<string> {
+    return await new Promise((resolve) => resolve(FAKE_HASH))
+  }
+}))
 
 describe('BcryptAdapter Tests', () => {
   it('should call bcrypt-adapter with correct values', async () => {
@@ -16,5 +24,14 @@ describe('BcryptAdapter Tests', () => {
     await sut.encrypt(fakePassword)
 
     expect(hashSpy).toHaveBeenCalledWith(fakePassword, 12)
+  })
+
+  it('should return a hash on bcrypt-adapter success', async () => {
+    const fakePassword = 'loremipsum123@#'
+
+    const sut = makeSut(12)
+    const resultHash = await sut.encrypt(fakePassword)
+
+    expect(resultHash).toEqual(FAKE_HASH)
   })
 })
