3978c4f66aaf43ede280bd67d488726f36b16790
brunosm16 <brunoskr23@gmail.com>
2023-01-10 09:13:18 -0300

    refactor: run mongo-helper on sign-up-routes

--- src/main/routes/sign-up-routes.test.ts
+++ src/main/routes/sign-up-routes.test.ts
@@ -1,9 +1,24 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 const SIGN_UP_URL = '/api/signup'
 
 describe('SignUp Route Tests', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accounts = MongoHelper.getCollectionByName('accounts')
+
+    await accounts.deleteMany({})
+  })
+
   it('should return statusCode 200 on success', async () => {
     const body = {
       name: 'Lorem Ipsum',
