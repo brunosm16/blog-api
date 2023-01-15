2b85c9ef054c36a4b80825b1e42d0c71ce9ae4f2
brunosm16 <brunoskr23@gmail.com>
2023-01-15 17:00:33 -0300

    refactor: uses log-controller-decorator on factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,6 +4,8 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { MongoAccountRepository } from '../../infra/db/mongodb/account-repository/mongo-account-repository'
 import { AddAccount } from '../../domain/usecases/add-account'
+import { Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from '../decorators/log-controller'
 
 const makeAddAccount = (): AddAccount => {
   const salt = 12
@@ -14,9 +16,10 @@ const makeAddAccount = (): AddAccount => {
   return new DbAddAccount(encrypter, mongoAccountRepository)
 }
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const addAccount = makeAddAccount()
   const emailValidator = new EmailValidatorAdapter()
+  const signUpController = new SignUpController(emailValidator, addAccount)
 
-  return new SignUpController(emailValidator, addAccount)
+  return new LogControllerDecorator(signUpController)
 }
