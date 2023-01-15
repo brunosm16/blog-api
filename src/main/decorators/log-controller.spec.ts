2c607e11da067f2197103dd693f9d4fd90b405e4
brunosm16 <brunoskr23@gmail.com>
2023-01-15 17:00:05 -0300

    test: log-controller-decorator calls controller with correct http-request value

---
+++ src/main/decorators/log-controller.spec.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from '../../presentation/protocols'
+import { LogControllerDecorator } from './log-controller'
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeControllerStub = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          ok: 'ok'
+        }
+      }
+
+      return await new Promise((resolve) => resolve(httpResponse))
+    }
+  }
+
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeControllerStub()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogControllerDecorator Tests', () => {
+  it('calls controller with correct http-request values', async () => {
+    const { sut, controllerStub } = makeSut()
+
+    const httpRequest = {
+      body: {
+        ok: 'ok'
+      }
+    }
+
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+
+    await sut.handle(httpRequest)
+
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
