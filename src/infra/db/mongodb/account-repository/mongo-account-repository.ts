a3086defb19c2eeeca30118b664890f6a98efb98
brunosm16 <brunoskr23@gmail.com>
2023-01-15 16:19:22 -0300

    refactor: reconnect mongodb when client is down

--- src/infra/db/mongodb/account-repository/mongo-account-repository.ts
+++ src/infra/db/mongodb/account-repository/mongo-account-repository.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class MongoAccountRepository implements AddAccountRepository {
   async add (addAccount: AddAccountModel): Promise<AccountModel> {
-    const collection = MongoHelper.getCollectionByName('accounts')
+    const collection = await MongoHelper.getCollectionByName('accounts')
 
     const { ops } = await collection.insertOne(addAccount)
 
