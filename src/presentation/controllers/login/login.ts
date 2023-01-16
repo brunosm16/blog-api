b3ea88493b6fe5b6da1ab7e59fb2f16bbbf64809
brunosm16 <brunoskr23@gmail.com>
2023-01-15 22:40:51 -0300

    feat: calls email-validator with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 import { makeBadRequest, makeOKRequest } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { body } = httpRequest
     const requiredFields = ['email', 'password']
@@ -11,6 +18,14 @@ export class LoginController implements Controller {
       if (!body[field]) {
         return makeBadRequest(new MissingParamError(field))
       }
+    }
+
+    const { email } = body
+
+    const emailIsValid = this.emailValidator.isValid(email)
+
+    if (!emailIsValid) {
+      return makeBadRequest(new InvalidParamError('email'))
     }
 
     return makeOKRequest({})
