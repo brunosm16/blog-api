bd4e2cb6de94d16b35d77ef984c88fab6bd6b29f
brunosm16 <brunoskr23@gmail.com>
2023-01-19 16:53:52 -0300

    test: add email-field-validation

---
+++ src/presentation/helpers/validators/email-field-validation.spec.ts
@@ -0,0 +1,77 @@
+import { AccountModel } from '../../controllers/signup/signup-protocols'
+import { InvalidParamError } from '../../errors'
+import { EmailValidator } from '../../protocols/email-validator'
+import { EmailFieldValidation } from './email-field-validation'
+
+interface SutTypes {
+  sut: EmailFieldValidation
+  emailValidatorStub: EmailValidator
+}
+
+const getFakeAccount = (): AccountModel => ({
+  id: 'fake-id',
+  name: 'lorem-ipsum',
+  email: 'loremipsum@email.com',
+  password: 'fake_hashed_password'
+})
+
+const makeValidatorStub = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  return new EmailValidatorStub()
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeValidatorStub()
+  const sut = new EmailFieldValidation('email', emailValidatorStub)
+
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
+
+describe('EmailFieldValidation', () => {
+  it('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const { email } = getFakeAccount()
+
+    sut.validate(getFakeAccount())
+
+    expect(isValidSpy).toHaveBeenCalledWith(email)
+  })
+
+  it('Should return error if email is invalid', () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+    const fakeAccount = {
+      id: 'fake-id',
+      name: 'lorem-ipsum',
+      email: 'loremipsum@',
+      password: 'fake_hashed_password'
+    }
+
+    const expectedError = new InvalidParamError('email')
+
+    const error = sut.validate(fakeAccount)
+
+    expect(error).toEqual(expectedError)
+  })
+
+  it('Should return null if email is valid', () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate(getFakeAccount())
+
+    expect(error).toEqual(null)
+  })
+})
