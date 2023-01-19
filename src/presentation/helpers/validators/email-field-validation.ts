7eddefc12b3662f558a71f32e89d76e07606d2d9
brunosm16 <brunoskr23@gmail.com>
2023-01-19 16:52:34 -0300

    feat: add email-field-validation

---
+++ src/presentation/helpers/validators/email-field-validation.ts
@@ -0,0 +1,21 @@
+import { InvalidParamError } from '../../errors'
+import { EmailValidator } from '../../protocols/email-validator'
+import { Validation } from '../validation'
+
+export class EmailFieldValidation implements Validation {
+  private readonly validator: EmailValidator
+  private readonly field: string
+
+  constructor (field: string, validator: EmailValidator) {
+    this.field = field
+    this.validator = validator
+  }
+
+  validate (input: any): Error | null {
+    if (!this.validator.isValid(input[this.field])) {
+      return new InvalidParamError('email')
+    }
+
+    return null
+  }
+}
