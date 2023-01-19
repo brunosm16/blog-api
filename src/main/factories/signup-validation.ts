7a1bf7a737d75a69610ba62c322d14767b22b172
brunosm16 <brunoskr23@gmail.com>
2023-01-19 15:50:28 -0300

    feat: add compare-fields-validation to sign-up-validation

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,4 +1,5 @@
 import { Validation } from '../../presentation/helpers/validation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 
@@ -10,6 +11,8 @@ const getValidationsArray = (): Validation[] => {
   for (const field of requiredFields) {
     validations.push(new RequiredFieldValidation(field))
   }
+
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirm'))
 
   return validations
 }
