6bba8af141c2700135570d25892a77a90f0017c4
brunosm16 <brunoskr23@gmail.com>
2023-01-21 18:43:02 -0300

    test: add tests to compare-fields-validation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,35 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (
+  field = 'fake_field',
+  fakeFieldToCompare = 'fake_field_to_compare'
+): CompareFieldsValidation => {
+  return new CompareFieldsValidation(field, fakeFieldToCompare)
+}
+
+describe('CompareFieldsValidation', () => {
+  it('should returns error if fields are no the same', () => {
+    const sut = makeSut()
+
+    const input = {
+      fake_field: 'lorem_ipsum',
+      fake_field_to_compare: 'integer_varius'
+    }
+
+    const error = sut.validate(input)
+    expect(error).toEqual(new InvalidParamError('fake_field_to_compare'))
+  })
+
+  it('should return true if fields are the same', () => {
+    const sut = makeSut()
+
+    const input = {
+      fake_field: 'lorem_ipsum',
+      fake_field_to_compare: 'lorem_ipsum'
+    }
+
+    const error = sut.validate(input)
+    expect(error).toEqual(null)
+  })
+})
