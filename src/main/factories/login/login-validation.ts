28ca0f782ebdbc56c2e27e7bfd9ca5d5259f016b
brunosm16 <brunoskr23@gmail.com>
2023-01-21 19:30:54 -0300

    feat: add login-validation factory

---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,25 @@
+import { Validation } from '../../../presentation/helpers/validation'
+import { EmailFieldValidation } from '../../../presentation/helpers/validators/email-field-validation'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+const requiredFields = ['email', 'password']
+
+const getValidationsArray = (): Validation[] => {
+  const validations: Validation[] = []
+
+  for (const field of requiredFields) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  validations.push(
+    new EmailFieldValidation('email', new EmailValidatorAdapter())
+  )
+
+  return validations
+}
+
+export const makeLoginValidation = (): ValidationComposite => {
+  return new ValidationComposite(getValidationsArray())
+}
