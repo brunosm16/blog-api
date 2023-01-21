841342939b24f7e76bda89eb159f4551478ce0d1
brunosm16 <brunoskr23@gmail.com>
2023-01-21 19:43:03 -0300

    feat: add login-validation on login-controller

---
+++ src/presentation/controllers/login/login-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../protocols'
+export * from '../../helpers/validation'
+export * from '../../../domain/usecases/authentication'
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,40 +1,35 @@
-import { Authentication } from '../../../domain/usecases/authentication'
-import { InvalidParamError, MissingParamError } from '../../errors'
 import {
   makeBadRequest,
   makeInternalServerError,
   makeOKRequest,
   makeUnauthorizedError
 } from '../../helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { EmailValidator } from '../../protocols/email-validator'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation,
+  Authentication
+} from '../login/login-protocols'
 
 export class LoginController implements Controller {
-  private readonly emailValidator: EmailValidator
+  private readonly validation: Validation
   private readonly authentication: Authentication
 
-  constructor (emailValidator: EmailValidator, authentication: Authentication) {
-    this.emailValidator = emailValidator
+  constructor (validation: Validation, authentication: Authentication) {
+    this.validation = validation
     this.authentication = authentication
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { body } = httpRequest
-      const requiredFields = ['email', 'password']
-
-      for (const field of requiredFields) {
-        if (!body[field]) {
-          return makeBadRequest(new MissingParamError(field))
-        }
-      }
-
       const { email, password } = body
 
-      const emailIsValid = this.emailValidator.isValid(email)
+      const error = this.validation.validate(body)
 
-      if (!emailIsValid) {
-        return makeBadRequest(new InvalidParamError('email'))
+      if (error) {
+        return makeBadRequest(error)
       }
 
       const accessToken = await this.authentication.auth(email, password)
