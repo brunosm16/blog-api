48336888189e44998838fdbe6e02d9d576475159
brunosm16 <brunoskr23@gmail.com>
2023-01-21 21:16:52 -0300

    feat: add db-authentication and ensure it calls load-account-by-email-repository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,16 @@
+import { AuthenticationModel } from '../../../domain/models/authentication'
+import { Authentication } from '../../../domain/usecases/authentication'
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+
+  async auth (authenticationModel: AuthenticationModel): Promise<string | null> {
+    await this.loadAccountByEmailRepository.load(authenticationModel.email)
+    return null
+  }
+}
