a4fa9e2b1091c28b1bd7a4dba96ded2beffbf30e
brunosm16 <brunoskr23@gmail.com>
2023-01-22 12:38:23 -0300

    feat: db-authentication returns token on success

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -3,6 +3,7 @@ import { Authentication } from '../../../domain/usecases/authentication'
 import { HashComparer } from '../../protocols/cryptography/hash-comparer'
 import { TokenGenerator } from '../../protocols/cryptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { AccountModel } from '../add-account/db-add-account-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
@@ -19,20 +20,44 @@ export class DbAuthentication implements Authentication {
     this.tokenGenerator = tokenGenerator
   }
 
+  async generateToken (id: string | undefined): Promise<string | null> {
+    if (!id) return null
+    return await this.tokenGenerator.generate(id)
+  }
+
+  async passwordIsValid (
+    password: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return await this.hashComparer.compare(password, hashedPassword)
+  }
+
+  async authIsValid (
+    account: AccountModel | null,
+    password: string
+  ): Promise<boolean> {
+    if (!account) return false
+
+    const { password: hashedPassword } = account
+
+    const validAuth = await this.hashComparer.compare(password, hashedPassword)
+
+    return validAuth
+  }
+
   async auth (authentication: AuthenticationModel): Promise<string | null> {
     const accountResult = await this.loadAccountByEmailRepository.load(
       authentication.email
     )
 
-    if (accountResult) {
-      await this.hashComparer.compare(
-        authentication.password,
-        accountResult.password
-      )
+    const validAuth = await this.authIsValid(
+      accountResult,
+      authentication.password
+    )
 
-      await this.tokenGenerator.generate(accountResult.id)
-    }
+    if (!validAuth) return null
 
-    return null
+    const token = await this.generateToken(accountResult?.id)
+    return token
   }
 }
