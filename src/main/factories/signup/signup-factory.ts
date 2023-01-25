4ec63db8345204bd66f5f5ac144193a77569ce72
brunosm16 <brunoskr23@gmail.com>
2023-01-25 10:11:39 -0300

    refactor: pass db-authentication to sign-up-factory

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,3 +1,4 @@
+import env from '../../config/env'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
@@ -7,6 +8,8 @@ import { Controller } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log-mongo-repository'
 import { makeSignUpValidation } from './signup-factory-validation'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 
 const makeAddAccount = (): AddAccount => {
   const salt = 12
@@ -17,11 +20,29 @@ const makeAddAccount = (): AddAccount => {
   return new DbAddAccount(hasher, mongoAccountRepository)
 }
 
+const makeBcryptAdapter = (salt = 12): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+const makeJwtAdapter = (): JwtAdapter => {
+  return new JwtAdapter(env.jwtSecret)
+}
+
+const makeDbAuthentication = (): DbAuthentication => {
+  return new DbAuthentication(
+    new MongoAccountRepository(),
+    new MongoAccountRepository(),
+    makeBcryptAdapter(),
+    makeJwtAdapter()
+  )
+}
+
 export const makeSignUpController = (): Controller => {
   const addAccount = makeAddAccount()
   const signUpController = new SignUpController(
     addAccount,
-    makeSignUpValidation()
+    makeSignUpValidation(),
+    makeDbAuthentication()
   )
   const logMongoRepository = new LogMongoRepository()
 
