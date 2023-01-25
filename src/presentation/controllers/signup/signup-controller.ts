ed739b18e71805b3cf00dadd1b6aa0c5716f540d
brunosm16 <brunoskr23@gmail.com>
2023-01-25 09:30:12 -0300

    feat: sign-up-controller calls authentication with correct values

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -4,6 +4,7 @@ import {
   makeOKRequest
 } from '../../helpers/http/http-helper'
 import { Validation } from '../../protocols/validation'
+import { Authentication } from '../login/login-controller-protocols'
 import {
   HttpRequest,
   HttpResponse,
@@ -14,7 +15,8 @@ import {
 export class SignUpController implements Controller {
   constructor (
     private readonly addAccount: AddAccount,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -28,6 +30,8 @@ export class SignUpController implements Controller {
       }
 
       const { name, email, password } = body
+
+      await this.authentication.auth({ email, password })
 
       const accountResult = await this.addAccount.add({ name, email, password })
 
