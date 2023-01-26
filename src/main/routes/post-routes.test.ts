4e9bb0742dee6b6fe45d88812c50db73996bdc16
brunosm16 <brunoskr23@gmail.com>
2023-01-26 13:05:21 -0300

    test: add post-routes

---
+++ src/main/routes/post-routes.test.ts
@@ -0,0 +1,44 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+const getFakeBody = (): any => ({
+  body: {
+    question: 'Lorem Ipsum',
+    answers: [
+      {
+        image: 'fake-image.png',
+        answer: 'fake_answer_1'
+      },
+      { answer: 'fake_answer_2' }
+    ]
+  }
+})
+
+const ADD_POST_URL = '/api/posts'
+
+describe('Post Route Tests', () => {
+  let accountCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollectionByName('posts')
+
+    await accountCollection.deleteMany({})
+  })
+
+  describe('POST /posts', () => {
+    it('should return 204 on post creation success', async () => {
+      const { body } = getFakeBody()
+      await request(app).post(ADD_POST_URL).send(body).expect(204)
+    })
+  })
+})
