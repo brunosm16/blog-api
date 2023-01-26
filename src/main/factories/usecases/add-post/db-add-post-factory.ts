0bda7bae63d014746e59ed220bb11628ef669855
brunosm16 <brunoskr23@gmail.com>
2023-01-26 10:20:52 -0300

    feat: implements db-add-post-factory

---
+++ src/main/factories/usecases/add-post/db-add-post-factory.ts
@@ -0,0 +1,6 @@
+import { DbAddPost } from '../../../../data/usecases/add-post/db-add-post'
+import { MongoPostRepository } from '../../../../infra/db/mongodb/post-repository/mongo-post-repository'
+
+export const makeDbAddPost = (): DbAddPost => {
+  return new DbAddPost(new MongoPostRepository())
+}
