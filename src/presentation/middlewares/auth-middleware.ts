13300c2ffa158c607460c53c4f8b66bb4ba14e16
brunosm16 <brunoskr23@gmail.com>
2023-01-31 15:06:05 -0300

    feat: auth-middleware returns 200 if load-account-by-token succeeds

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,17 +1,26 @@
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import { AccountModel } from '../controllers/signup/signup-controller-protocols'
 import { AccessDeniedError } from '../errors'
-import { makeForbiddenError } from '../helpers/http/http-helper'
+import { makeForbiddenError, makeOKRequest } from '../helpers/http/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols'
 import { Middleware } from '../protocols/middleware'
 
 export class AuthMiddleware implements Middleware {
   constructor (private readonly loadAccountByToken: LoadAccountByToken) {}
 
+  async getAccount (accessToken: string): Promise<AccountModel | null> {
+    if (!accessToken) return null
+
+    return await this.loadAccountByToken.load(accessToken)
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest?.headers?.['x-access-token']
 
-    if (accessToken) {
-      await this.loadAccountByToken.load(accessToken)
+    const account = await this.getAccount(accessToken)
+
+    if (account) {
+      return makeOKRequest({ accountId: account.id })
     }
 
     const error = makeForbiddenError(new AccessDeniedError())
