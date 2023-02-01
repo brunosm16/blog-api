75d7e41e13fc79e69d26617237a00f9d876843f9
brunosm16 <brunoskr23@gmail.com>
2023-02-01 09:26:02 -0300

    feat: db-load-account-by-token-repository called with correct values

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,14 +1,22 @@
 import {
   LoadAccountByToken,
+  LoadAccountByTokenRepository,
   Decrypter,
   AccountModel
 } from './db-load-account-by-token-protocols'
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
-  constructor (private readonly decrypter: Decrypter) {}
+  constructor (
+    private readonly decrypter: Decrypter,
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+  ) {}
 
   async load (accessToken: string): Promise<AccountModel | null> {
-    await this.decrypter.decrypt(accessToken)
+    const token = await this.decrypter.decrypt(accessToken)
+
+    if (token) {
+      await this.loadAccountByTokenRepository.loadByToken(token)
+    }
 
     return null
   }
