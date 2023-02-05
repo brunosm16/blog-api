7ffa6ffb90c50ffe1238d28ee4f414eb74eecedd
brunosm16 <brunoskr23@gmail.com>
2023-02-05 18:54:23 -0300

    feat: mongo-post-repository returns posts on load-by-id success

--- src/infra/db/mongodb/post-repository/mongo-post-repository.ts
+++ src/infra/db/mongodb/post-repository/mongo-post-repository.ts
@@ -1,14 +1,15 @@
+import { LoadPostByIdRepository } from '@/data/usecases/load-post-by-id/db-load-post-by-id-protocols'
 import { Collection } from 'mongodb'
-import { LoadPostsRepository } from '../../../../data/protocols/db/post/load-posts-repository'
+import { LoadPostsRepository } from '@/data/protocols/db/post/load-posts-repository'
 import {
   AddPostModel,
   AddPostRepository
-} from '../../../../data/usecases/add-post/db-add-post-protocols'
-import { PostModel } from '../../../../domain/models/post'
-import { MongoHelper } from '../helpers/mongo-helper'
+} from '@/data/usecases/add-post/db-add-post-protocols'
+import { PostModel } from '@/domain/models/post'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
 export class MongoPostRepository
-implements AddPostRepository, LoadPostsRepository {
+implements AddPostRepository, LoadPostsRepository, LoadPostByIdRepository {
   async getPostsCollection (): Collection {
     const postsCollection = await MongoHelper.getCollectionByName('posts')
 
@@ -24,8 +25,16 @@ implements AddPostRepository, LoadPostsRepository {
   async loadAll (): Promise<PostModel[]> {
     const postsCollection = await this.getPostsCollection()
 
-    const posts: PostModel[] = await postsCollection.find().toArray()
+    const posts = await postsCollection.find().toArray()
 
     return posts
+  }
+
+  async loadById (id: string): Promise<PostModel> {
+    const postsCollection = await this.getPostsCollection()
+
+    const post = await postsCollection.findOne({ _id: id })
+
+    return post
   }
 }
