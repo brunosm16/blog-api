c14770231011b2b6a0e7dbc4b85308f1a1cc5505
brunosm16 <brunoskr23@gmail.com>
2023-02-04 22:15:27 -0300

    test: mongo-post-repository returns posts on load-all success

--- src/infra/db/mongodb/post-repository/mongo-post-repository.spec.ts
+++ src/infra/db/mongodb/post-repository/mongo-post-repository.spec.ts
@@ -8,16 +8,28 @@ interface SutTypes {
   sut: MongoPostRepository
 }
 
-const getFakePost = (): AddPostModel => ({
-  question: 'fake_question',
-  answers: [
-    {
-      image: 'fake_image',
-      answer: 'fake_answer'
-    }
-  ],
-  date: new Date()
-})
+const getFakePosts = (): AddPostModel[] => [
+  {
+    question: 'fake_question',
+    answers: [
+      {
+        image: 'fake_image',
+        answer: 'fake_answer'
+      }
+    ],
+    date: new Date()
+  },
+  {
+    question: 'fake_question_2',
+    answers: [
+      {
+        image: 'fake_image_2',
+        answer: 'fake_answer_2'
+      }
+    ],
+    date: new Date()
+  }
+]
 
 const makeSut = (): SutTypes => {
   return {
@@ -48,11 +60,27 @@ describe('MongoPostRepository', () => {
     it('should add a new post on add-post success', async () => {
       const { sut } = makeSut()
 
-      await sut.add(getFakePost())
+      const post = getFakePosts()
+
+      await sut.add(post[0])
 
       const result = await postCollection.findOne({ question: 'fake_question' })
 
       expect(result).toBeTruthy()
+    })
+  })
+
+  describe('.load', () => {
+    it('should return posts on loadAll success', async () => {
+      const { sut } = makeSut()
+
+      await postCollection.insertMany(getFakePosts())
+
+      const posts = await sut.loadAll()
+
+      expect(posts.length).toEqual(2)
+      expect(posts[0].question).toEqual('fake_question')
+      expect(posts[1].question).toEqual('fake_question_2')
     })
   })
 })
