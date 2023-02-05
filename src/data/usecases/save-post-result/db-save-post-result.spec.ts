9180e2af05c88708a312a0afaf393bae51c4375a
brunosm16 <brunoskr23@gmail.com>
2023-02-05 17:43:21 -0300

    test: db-save-post-result calls save

---
+++ src/data/usecases/save-post-result/db-save-post-result.spec.ts
@@ -0,0 +1,53 @@
+import { DbSavePostResult } from './db-save-post-result'
+import {
+  SavePostResultRepository,
+  PostResultModel,
+  SavePostResultModel
+} from './db-save-post-result-protocols'
+
+type SutTypes = {
+  sut: DbSavePostResult
+  savePostResultRepositoryStub: SavePostResultRepository
+}
+
+const makeSavePostResultModel = (): SavePostResultModel => ({
+  postId: 'fake_postId',
+  accountId: 'fake_accountId',
+  answer: 'fake_answer',
+  date: new Date()
+})
+
+const makeFakePostResultModel = (): PostResultModel =>
+  Object.assign({}, makeSavePostResultModel(), { id: 'fake_id' })
+
+const makeSavePostResultRepository = (): SavePostResultRepository => {
+  class SavePostResultRepositoryStub implements SavePostResultRepository {
+    async save (data: SavePostResultModel): Promise<PostResultModel> {
+      return await new Promise((resolve) => resolve(makeFakePostResultModel()))
+    }
+  }
+
+  return new SavePostResultRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const savePostResultRepositoryStub = makeSavePostResultRepository()
+  const sut = new DbSavePostResult(savePostResultRepositoryStub)
+
+  return {
+    sut,
+    savePostResultRepositoryStub
+  }
+}
+
+describe('DbSavePostResult', () => {
+  it('should call save on db-save-post-result', async () => {
+    const { sut, savePostResultRepositoryStub } = makeSut()
+
+    const saveSpy = jest.spyOn(savePostResultRepositoryStub, 'save')
+
+    await sut.save(makeFakePostResultModel())
+
+    expect(saveSpy).toHaveBeenCalled()
+  })
+})
