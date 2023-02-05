53cb8006a9484d07c88df4e28eb7232d7001fe0c
brunosm16 <brunoskr23@gmail.com>
2023-02-04 22:15:06 -0300

    feat: mongo-post-repository returns posts on load-all success

--- src/infra/db/mongodb/post-repository/mongo-post-repository.ts
+++ src/infra/db/mongodb/post-repository/mongo-post-repository.ts
@@ -1,13 +1,31 @@
+import { Collection } from 'mongodb'
+import { LoadPostsRepository } from '../../../../data/protocols/db/post/load-posts-repository'
 import {
   AddPostModel,
   AddPostRepository
 } from '../../../../data/usecases/add-post/db-add-post-protocols'
+import { PostModel } from '../../../../domain/models/post'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class MongoPostRepository implements AddPostRepository {
-  async add (postData: AddPostModel): Promise<void> {
+export class MongoPostRepository
+implements AddPostRepository, LoadPostsRepository {
+  async getPostsCollection (): Collection {
     const postsCollection = await MongoHelper.getCollectionByName('posts')
 
+    return postsCollection
+  }
+
+  async add (postData: AddPostModel): Promise<void> {
+    const postsCollection = await this.getPostsCollection()
+
     await postsCollection.insertOne(postData)
+  }
+
+  async loadAll (): Promise<PostModel[]> {
+    const postsCollection = await this.getPostsCollection()
+
+    const posts: PostModel[] = await postsCollection.find().toArray()
+
+    return posts
   }
 }
