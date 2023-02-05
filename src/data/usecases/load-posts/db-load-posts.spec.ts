a359a7e767af7e363521000d758c0042a8e39de1
brunosm16 <brunoskr23@gmail.com>
2023-02-04 21:52:28 -0300

    test: db-load-posts calls load-posts-repository

---
+++ src/data/usecases/load-posts/db-load-posts.spec.ts
@@ -0,0 +1,67 @@
+import { PostModel } from '../../../domain/models/post'
+import { LoadPostsRepository } from '../../protocols/db/post/load-posts-repository'
+import { DbLoadPosts } from './db-load-posts'
+
+interface SutTypes {
+  sut: DbLoadPosts
+  loadPostsRepositoryStub: LoadPostsRepository
+}
+
+const makeFakePosts = (): PostModel[] => {
+  return [
+    {
+      id: 'fake_post_id_1',
+      question: 'fake_question_1',
+      answers: [
+        {
+          image: 'fake_image_1',
+          answer: 'fake_answer_1'
+        }
+      ],
+      date: new Date()
+    },
+    {
+      id: 'fake_post_id_2',
+      question: 'fake_question_2',
+      answers: [
+        {
+          image: 'fake_image_2',
+          answer: 'fake_answer_2'
+        }
+      ],
+      date: new Date()
+    }
+  ]
+}
+
+const makeLoadPostsRepository = (): LoadPostsRepository => {
+  class LoadPostsRepositoryStub implements LoadPostsRepository {
+    async loadAll (): Promise<PostModel[]> {
+      return await new Promise((resolve) => resolve(makeFakePosts()))
+    }
+  }
+
+  return new LoadPostsRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadPostsRepositoryStub = makeLoadPostsRepository()
+  const sut = new DbLoadPosts(loadPostsRepositoryStub)
+
+  return {
+    sut,
+    loadPostsRepositoryStub
+  }
+}
+
+describe('DbLoadPosts Repository', () => {
+  it('should call load-posts-repository', async () => {
+    const { sut, loadPostsRepositoryStub } = makeSut()
+
+    const loadAllSpy = jest.spyOn(loadPostsRepositoryStub, 'loadAll')
+
+    await sut.load()
+
+    expect(loadAllSpy).toHaveBeenCalled()
+  })
+})
