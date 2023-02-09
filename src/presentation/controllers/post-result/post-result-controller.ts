a0dee625e7db5c6fde732de2545268cfa34d5d2c
brunosm16 <brunoskr23@gmail.com>
2023-02-09 11:42:42 -0300

    feat: post-result-controller should return 403 when answer is invalid

--- src/presentation/controllers/post-result/post-result-controller.ts
+++ src/presentation/controllers/post-result/post-result-controller.ts
@@ -1,6 +1,7 @@
 import {
   InvalidParamError,
-  makeInternalServerError
+  makeInternalServerError,
+  PostModel
 } from '../post/load-posts/load-posts-controller-protocols'
 import {
   Controller,
@@ -14,14 +15,30 @@ import {
 export class PostResultController implements Controller {
   constructor (private readonly loadPostById: LoadPostById) {}
 
+  validateAnswer (post: PostModel | null, answer: string | null): Error | null {
+    if (!post) {
+      return new InvalidParamError('postId')
+    }
+
+    const answers = post.answers.map(({ answer }) => answer)
+
+    if (!answer || !answers.includes(answer)) {
+      return new InvalidParamError('answer')
+    }
+
+    return null
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const { params } = httpRequest
+      const { params, body } = httpRequest
 
       const post = await this.loadPostById.loadById(params.id)
 
-      if (!post) {
-        return makeForbiddenError(new InvalidParamError('postId'))
+      const error = this.validateAnswer(post, body?.answer)
+
+      if (error) {
+        return makeForbiddenError(error)
       }
 
       return makeOKRequest(post)
